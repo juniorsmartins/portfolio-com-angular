182ddd3cd2e98853ecccf217a4f8851becb43094
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-10-06 17:12:07 -0400

    Construção do componente de criar cursos.

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CriarComponent } from './componentes/cursos/criar/criar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    CriarComponent
   ],
   imports: [
     BrowserModule,
