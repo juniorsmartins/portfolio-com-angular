c05a15ac183995b2697e4c7257969544d7fa1b31
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-10-14 18:30:22 -0400

    Conclusão da atividade 2 da disciplina Frameworks Web.

---
+++ UTFPR-Atividade2/atividade2.js
@@ -0,0 +1,60 @@
+function startPage() {
+    console.log("Bem-vindo!")
+}
+startPage();
+
+
+var botaoPegarValor = document.querySelector("#btnPegarValor");
+
+botaoPegarValor.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    let formulario = document.querySelector("#formulario-valor");
+    console.log(formulario.valor.value);
+    calcularNumeroDeNotasPorValor(formulario.valor.value);
+})
+
+function calcularNumeroDeNotasPorValor(valorDeSaque) {
+
+    let umReal = 0;
+    let doisReais = 0;
+    let cincoReais = 0;
+    let dezReais = 0;
+    let vinteReais = 0;
+    let cinquentaReais = 0;
+    let cemReais = 0;
+
+    while (valorDeSaque != null && valorDeSaque >= 1) {
+        if (valorDeSaque >= 100) {
+            valorDeSaque -= 100;
+            cemReais++;
+        } else if (valorDeSaque >= 50) {
+            valorDeSaque -= 50;
+            cinquentaReais++;
+        } else if (valorDeSaque >= 20) {
+            valorDeSaque -= 20;
+            vinteReais++;
+        } else if (valorDeSaque >= 10) {
+            valorDeSaque -= 10;
+            dezReais++;
+        } else if (valorDeSaque >= 5) {
+            valorDeSaque -= 5;
+            cincoReais++;
+        } else if (valorDeSaque >= 2) {
+            valorDeSaque -= 2;
+            doisReais++;
+        } else if (valorDeSaque = 1) {
+            valorDeSaque -= 1;
+            umReal++;
+        }
+    }
+
+    document.getElementById("um").innerHTML = "R$ 1,00: " + umReal;
+    document.getElementById("dois").innerHTML = "R$ 2,00: " + doisReais;
+    document.getElementById("cinco").innerHTML = "R$ 5,00: " + cincoReais;
+    document.getElementById("dez").innerHTML = "R$ 10,00: " + dezReais;
+    document.getElementById("vinte").innerHTML = "R$ 20,00: " + vinteReais;
+    document.getElementById("cinquenta").innerHTML = "R$ 50,00: " + cinquentaReais;
+    document.getElementById("cem").innerHTML = "R$ 100,00: " + cemReais;
+}
+
