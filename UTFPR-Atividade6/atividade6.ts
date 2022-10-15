d0b607cb2c726c033cc39b4e03e07f77825c3465
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-10-15 12:16:37 -0400

    Conclusão da atividade 6

--- UTFPR-Atividade6/atividade6.ts
+++ UTFPR-Atividade6/atividade6.ts
@@ -1,21 +1,29 @@
 const tipoGasolina = 'GASOLINA';
 const tipoEtanol = 'ETANOL';
 const parametroDaVantagem = 0.7;
+
 class Posto {
-    constructor(nome, gasolina, etanol) {
+
+    private nome: string;
+    private gasolina: Combustivel;
+    private etanol: Combustivel;
+
+    constructor(nome: string, gasolina: Combustivel, etanol: Combustivel) {
         this.nome = nome;
         this.gasolina = gasolina;
         this.etanol = etanol;
     }
+
     sugereCombustivel() {
-        var sugestaoDeCombustivel;
-        let porcentagem = this.etanol.preco / this.gasolina.preco;
-        if (porcentagem < parametroDaVantagem) {
+        var sugestaoDeCombustivel: String;
+        let porcentagem: number = this.etanol.preco / this.gasolina.preco;
+
+        if(porcentagem < parametroDaVantagem) {
             sugestaoDeCombustivel = tipoEtanol;
-        }
-        else {
+        } else {
             sugestaoDeCombustivel = tipoGasolina;
         }
+
         return `\n TABELA DE PREÇOS DE COMBUSTÍVEIS \n 
             Nome do posto: ${this.nome} \n
             Gasolina: ${this.gasolina.preco} \n 
@@ -24,16 +32,25 @@ class Posto {
             Combustível vantajoso: ${sugestaoDeCombustivel} \n`;
     }
 }
+
 class Combustivel {
-    constructor(tipo, preco) {
+
+    public tipo: string;
+    public preco: number;
+
+    constructor(tipo: string, preco: number) {
         this.tipo = tipo;
         this.preco = preco;
     }
 }
+
 function startMain() {
+
     var gasolina = new Combustivel(tipoGasolina, 6.799);
     var etanol = new Combustivel(tipoEtanol, 4.299);
     var posto = new Posto('Posto Ipiranga', gasolina, etanol);
+
     console.log(posto.sugereCombustivel());
 }
+
 startMain();
