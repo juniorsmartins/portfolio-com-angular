12b4acccb79d76db6676ca58cca8f85934f6e84d
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-10-15 14:49:37 -0400

    Remoção de módulos desnecessários ou duplicados.

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
-import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarComponent } from './componentes/cursos/criar/criar.component';
 import { FormsModule } from '@angular/forms';
 import { Atividade7CabecalhoComponent } from './componentes/atividade7-cabecalho/atividade7-cabecalho.component';
@@ -13,8 +11,6 @@ import { Atividade7RodapeComponent } from './componentes/atividade7-rodape/ativi
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent,
-    RodapeComponent,
     CriarComponent,
     Atividade7CabecalhoComponent,
     Atividade7RodapeComponent
