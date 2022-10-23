4a54f5a21b1e5b438c099b7d4a2ff46f64159533
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-10-23 10:07:28 -0400

    Início da construção do Listar Cursos.

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FormsModule } from '@angular/forms';
 import { Atividade7CabecalhoComponent } from './componentes/atividade7-cabecalho/atividade7-cabecalho.component';
 import { Atividade7RodapeComponent } from './componentes/atividade7-rodape/atividade7-rodape.component';
 import { CadastrarCursosComponent } from './componentes/especificos/cadastrar-cursos/cadastrar-cursos.component';
+import { ListarCursosComponent } from './componentes/especificos/listar-cursos/listar-cursos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Atividade7CabecalhoComponent,
     Atividade7RodapeComponent,
-    CadastrarCursosComponent
+    CadastrarCursosComponent,
+    ListarCursosComponent
   ],
   imports: [
     BrowserModule,
