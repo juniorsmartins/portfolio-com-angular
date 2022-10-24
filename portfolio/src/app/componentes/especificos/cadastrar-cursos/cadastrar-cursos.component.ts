c4f48b4e1f16e30c3745d86138adcc3da94da1e0
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-10-24 18:20:29 -0400

    Refinamento de Cadastrar e Listar.

--- portfolio/src/app/componentes/especificos/cadastrar-cursos/cadastrar-cursos.component.ts
+++ portfolio/src/app/componentes/especificos/cadastrar-cursos/cadastrar-cursos.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class CadastrarCursosComponent implements OnInit {
 
     chapeus = {
-      formulario: 'Formulário de cadastro',
+      formulario: 'Formulário para cadastrar cursos',
       ficha: 'Ficha'
   }
 
--- portfolio/src/app/componentes/especificos/item-cursos/item-cursos.component.ts
+++ portfolio/src/app/componentes/especificos/item-cursos/item-cursos.component.ts
@@ -20,7 +20,10 @@ export class ItemCursosComponent implements OnInit {
   ngOnInit(): void {}
 
   larguraCardCurso(): string {
-    return '';
+    if(this.curso.titulo.length >= 256) {
+      return 'nome-de-estilo-conforme-tamanho-aqui-e-grande';
+    }
+    return 'nome-de-estilo-conforme-tamanho-aqui-e-pequeno';
   }
 
   editarCadastroDoCurso() {
@@ -30,4 +33,9 @@ export class ItemCursosComponent implements OnInit {
   deletarCadastroDoCurso() {
     alert("Curso deletado com sucesso!");
   }
+
+  detalharCadastroDoCurso() {
+    alert("Curso detalhado com sucesso!");
+  }
+
 }
