5d44d11dcd7ec17bfc120173662fc43c99ef147d
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-10-24 18:30:08 -0400

    Adição de interpolação no rodapé. Para gerar ano dinâmico na data de direitos reservados.

--- portfolio/src/app/componentes/atividade7-rodape/atividade7-rodape.component.ts
+++ portfolio/src/app/componentes/atividade7-rodape/atividade7-rodape.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Atividade7RodapeComponent implements OnInit {
 
-  constructor() { }
+  ano: number;
+
+  constructor() {
+    this.ano = new Date().getFullYear();
+  }
 
   ngOnInit(): void {
   }
