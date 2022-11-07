3dc46bc3c57381cebb87787c368bdd4bdcbc5ffa
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-11-07 09:41:54 -0400

    Add LocalStorage na página de login.

--- portfolio/src/app/componentes/login/login.component.ts
+++ portfolio/src/app/componentes/login/login.component.ts
@@ -11,7 +11,30 @@ export class LoginComponent implements OnInit {
     login: 'Security'
   }
 
+  usuario = {
+    login: '',
+    senha: ''
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  salvarLoginLocalStorage() {
+    localStorage.setItem("usuario", JSON.stringify({login: this.usuario.login, senha: this.usuario.senha}));
+    localStorage.setItem('login', this.usuario.login);
+    localStorage.setItem('senha', this.usuario.senha);
+
+    console.log(JSON.parse(localStorage.getItem('usuario')!)); // publicado como objeto
+    console.log(localStorage.getItem('usuario')); // publicado como string
+    console.log(localStorage.getItem('login')); // string
+    console.log(localStorage.getItem('senha')); // string
+
+    this.limparFormulario();
+  }
+
+  limparFormulario() {
+    this.usuario.login = '',
+    this.usuario.senha = ''
+  }
 }
